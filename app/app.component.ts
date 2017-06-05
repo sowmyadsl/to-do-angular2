be9d725af2a56e64a85fedd84c85da3f3ac804ef
Kat Gifford and Sowmya Dinavahi <kat@>
2017-06-05 11:00:17 -0700

    add current time/date

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>My First Angular 2 App</h1>
+    <h1>TO-DO List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
   </div>
   `
 })
 
 export class AppComponent {
-
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
 }

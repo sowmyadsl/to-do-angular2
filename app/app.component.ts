050d66bb7801b380e2dc452daff098b282fffd80
Sowmya Dinavahi <sowmya.dsl@gmail.com>
2017-06-06 17:49:28 -0700

    Add stuff to show color on tasks and set priority

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,12 +4,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>TO-DO List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <ul>
-      <p *ngFor="let currentTask of tasks">{{currentTask.description}}</p>
-    </ul>
+       <div class="container">
+      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
+      <ul>
+        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+      </ul>
+      <hr>
+    <div>
+      <div *ngIf="selectedTask">
+         <h3>{{selectedTask.description}}</h3>
+         <p>Task Complete? {{selectedTask.done}}</p>
+         <hr>
+        <h3>Edit Task</h3>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+         <label>Enter Task Priority (1-3):</label>
+         <br>
+         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority
+          <button (click)="finishedEditing()">Done</button>
+      </div>
+    </div>
   </div>
   `
 })
@@ -23,16 +40,39 @@ export class AppComponent {
   year: number = this.currentTime.getFullYear();
 
   tasks: Task[] = [
-    new Task ("complete angular weekday hmwk"),
-    new Task ("add README files to github Repos"),
-    new Task ("start thinking about possible group projects")
+    new Task ("Complete angular weekday hmwk", 3),
+    new Task ("Add README files to github Repos", 2),
+    new Task ("Start thinking about possible group projects", 2)
   ];
+  selectedTask = null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  isDone(clickedTask: Task){
+    if(clickedTask.done === true){
+      alert("This task is done!")
+    } else {
+      alert("this is not done.Better get to work!");
+    }
+  }
+
+  priorityColor(currentTask){
+    if(currentTask.priority === 3){
+      return "bg-danger";
+    }else if (currentTask.priority === 2){
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
 
 
 ///////Model Declaration////////
 export class Task {
   public done: boolean = false;
-  constructor(public description: string) {
+  constructor(public description: string, public priority: number) {
   }
 }
